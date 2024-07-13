bdf66304b243283934cf7fc750230759a26c6bea
Isaac-Tolu <isaactoluwani30@gmail.com>
2024-07-13 11:02:43 +0100

    create item fields

--- haypp/haypp/items.py
+++ haypp/haypp/items.py
@@ -3,10 +3,24 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field, Item
 
 
-class HayppItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class HayppItem(Item):
+    brand_name = Field()
+    product_type = Field()
+    format = Field()
+    strength = Field()
+    flavour_facts = Field()
+    nicotine_strength_mg_pouch = Field()
+    nicotine_strength_mg_g = Field()
+    pouches_per_can = Field()
+    weight_per_pouch_gram = Field()
+    content_per_can_gram = Field()
+    producer = Field()
+    category = Field()
+    flavour_pg = Field()
+    strength_definition = Field()
+    manufacturer = Field()
+    nicotine_mg_portion = Field()
+    amount_of_bags_can = Field()
